767cd9e5738cf8d6ca4166a9a25cdda2cdef2185
tfitzner <5036869+tfitzner@users.noreply.github.com>
2022-10-04 17:41:28 -0400

    fixed syntax errors

--- Program.cs
+++ Program.cs
@@ -6,6 +6,43 @@ namespace PasswordChecker
   {
     public static void Main(string[] args)
     {
+      int minLength = 8;
+      string uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+      string lowercase = "abcdefghijklmnopqrstuvwxyz";
+      string digits = "0123456789";
+      string specialChars = "!@#$%^&*()_";
+
+      int score = 0;
+
+      Console.WriteLine("Enter a password...");
+      string password = Console.ReadLine();
+
+      if (password.Length >= 8)
+      {
+        score++;
+      }
+
+      if (Tools.Contains(password, uppercase))
+      {
+        score++;
+      }
+
+      if (Tools.Contains(password, lowercase))
+      {
+        score++;
+      }
+
+      if (Tools.Contains(password, digits))
+      {
+        score++;
+      }
+      if (Tools.Contains(password, specialChars))
+      {
+        score++;
+      }
+
+      Console.WriteLine(score);
+
 
     }
   }
